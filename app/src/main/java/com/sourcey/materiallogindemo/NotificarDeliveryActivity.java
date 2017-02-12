3a59e00e9d9a86163fd4d2eda07d065248703f6e
Alejandro Levinas <levinasale@gmail.com>
2017-02-12 19:50:51 -0300

    Agregada pantalla de carga de estado de delivery

--- app/src/main/java/com/sourcey/materiallogindemo/NotificarDeliveryActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/NotificarDeliveryActivity.java
@@ -1,8 +1,13 @@
 package com.sourcey.materiallogindemo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,11 +17,56 @@ import butterknife.ButterKnife;
  */
 
 public class NotificarDeliveryActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    @Bind(R.id.imageView1)
+    ImageView _imagenFoto;
+    @Bind(R.id.buttonEntregado)
+    Button botonEntregado;
+    @Bind(R.id.buttonNoEntregado)
+    Button _botonNoEntregado;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_notificar);
         ButterKnife.bind(this);
+
+        botonEntregado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                entregadoApretado();
+            }
+
+
+        });
+
+        _botonNoEntregado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noEntregadoApretado();
+            }
+
+        });
+        tomarFoto();
+    }
+
+    private void noEntregadoApretado() {
+
+    }
+
+    private void entregadoApretado() {
+
+    }
+
+    private void tomarFoto(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            _imagenFoto.setImageBitmap(photo);
+        }
     }
 
 }
