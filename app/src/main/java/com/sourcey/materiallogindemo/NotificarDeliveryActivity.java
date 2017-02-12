000fea99dc063de62f01831dbc1d79248a98db3e
Alejandro Levinas <levinasale@gmail.com>
2017-02-12 20:18:45 -0300

    Agregado un dialogo para cuando se apreta NO ENTREGADO

--- app/src/main/java/com/sourcey/materiallogindemo/NotificarDeliveryActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/NotificarDeliveryActivity.java
@@ -1,12 +1,16 @@
 package com.sourcey.materiallogindemo;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import butterknife.Bind;
@@ -44,17 +48,59 @@ public class NotificarDeliveryActivity extends AppCompatActivity {
             public void onClick(View v) {
                 noEntregadoApretado();
             }
-
         });
         tomarFoto();
     }
 
-    private void noEntregadoApretado() {
-
+    private void entregadoApretado() {
+        finish();
     }
 
-    private void entregadoApretado() {
+    private void noEntregadoApretado() {
+        CharSequence opciones[] = new CharSequence[] {"No atienden en el domicilio", "Cliente no quiso aceptarlo",
+                "No se pudo llegar a horario al domicilio", "Otro"};
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("¿Por qué no pudo ser entregado?");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 3)
+                    otroApretado();
+                else
+                    finish();
+            }
+        });
+        builder.show();
+    }
+
+    private void otroApretado() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        final EditText edittext = new EditText(this.getBaseContext());
+        alert.setTitle("Delivery no entregado");
+        alert.setMessage("Describa por qué no puedo realizar la entrega");
+
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //What ever you want to do with the value
+                //Editable YouEditTextValue = edittext.getText();
+                //OR
+                String YouEditTextValue = edittext.getText().toString();
+                finish();
+            }
+        });
+
+        alert.setNegativeButton("Descartar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                finish();
+            }
+        });
+
+        alert.show();
     }
 
     private void tomarFoto(){
